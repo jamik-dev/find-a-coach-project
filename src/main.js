bdafa9ff6a416bb062ab3eff4952f9451ea8e534
jamik-dev <jamik.oik.group@gmail.com>
2022-11-12 20:39:47 +0500

    adding base ui components

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import App from './App.vue'
 import store from './store/index.js'
 //importing router navigation
 import router from './router.js'
-
+//importing global components
+import BaseCard from '@/components/ui/BaseCard.vue';
+import BaseButton from '@/components/ui/BaseButton.vue';
+import BaseBadge from '@/components/ui/BaseBadge.vue';
 // creating App
-createApp(App).use(router).use(store).mount('#app');
+createApp(App).use(router).use(store).component('base-card', BaseCard).component('base-button', BaseButton).component('base-badge', BaseBadge).mount('#app');
