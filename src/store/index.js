89983e5e45d6459ea5cc646376cc244ffde3d239
jamik-dev <jamik.oik.group@gmail.com>
2022-11-13 17:42:52 +0500

    contact coach page has been done

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore } from "vuex";
 
 import coachesModule from "./modules/coaches/index.js";
+import requestsModule from "./modules/requests/index.js";
 
 const store = createStore({
   modules: {
-    coaches: coachesModule
+    coaches: coachesModule,
+    requests: requestsModule
   },
   state() {
     return {
