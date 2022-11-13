5013c812ec05d4b2129301ec954dc8476d9097db
jamik-dev <jamik.oik.group@gmail.com>
2022-11-13 11:59:00 +0500

    adding register and form pages

--- src/pages/coaches/CoachDetails.vue
+++ src/pages/coaches/CoachDetails.vue
@@ -40,7 +40,7 @@ export default {
       return this.selectedCoach.firstName + ' ' + this.selectedCoach.lastName;
     },
     contactLink() {
-      return this.$route.path + '/' + this.id + '/contact';
+      return this.$route.path + '/contact';
     },
     areas() {
       return this.selectedCoach.areas;
--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,13 +1,20 @@
 <template>
-  <h1>REGISTRATION</h1>
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <coach-form></coach-form>
+    </base-card>
+  </section>
 </template>
 
 <script>
+import CoachForm from "@/components/coaches/CoachForm.vue";
 export default {
-
-}
+  components: {
+    CoachForm,
+  },
+};
 </script>
 
 <style>
-
 </style>
