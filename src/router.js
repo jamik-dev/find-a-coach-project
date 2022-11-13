7ac857353d905f802769654272981fe6ed55eec3
jamik-dev <jamik.oik.group@gmail.com>
2022-11-13 06:36:12 +0500

    coachDetail page done

--- src/router.js
+++ src/router.js
@@ -16,6 +16,7 @@ const router = createRouter({
     {
       path: "/coaches/:id",
       component: CoachDetails,
+      props: true,
       children: [
         { path: "contact", component: ContactCoach }, // /coaches/c1/contact
       ],
